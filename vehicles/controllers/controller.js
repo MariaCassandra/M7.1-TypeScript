a0979ed8f4e966389115e74d2743f22365144b6f
Maria Cassandra <73235842+MariaCassandra@users.noreply.github.com>
2021-05-11 22:16:47 +0200

    Update controller.js

--- vehicles/controllers/controller.js
+++ vehicles/controllers/controller.js
@@ -1,5 +1,5 @@
 "use strict";
-var car = new Car();
+var car = new Car("", "", "");
 var wheelsForm = document.getElementsByClassName("wheels-form")[0];
 function showCar(car) {
     var divCar = document.getElementById("car-container");
@@ -25,45 +25,85 @@ function createCar() {
     var inputBrand = document.getElementById("inputBrand").value;
     var inputPlateDiv = document.getElementById("inputPlate");
     var inputPlateError = document.getElementById("plateError");
-    if (validatePlate(inputPlate)) {
+    var inputBrandDiv = document.getElementById("inputBrand");
+    var inputBrandError = document.getElementById("brandError");
+    var inputColorDiv = document.getElementById("inputColor");
+    var inputColorError = document.getElementById("colorError");
+    var carValidator = true;
+    if (inputBrand == "") {
+        inputBrandDiv.classList.add("is-invalid");
+        inputBrandError.textContent = "This field is mandatory";
+        carValidator = false;
+    }
+    else {
+        inputBrandDiv.classList.remove("is-invalid");
+        inputBrandError.textContent = "";
+    }
+    if (inputColor == "") {
+        inputColorDiv.classList.add("is-invalid");
+        inputColorError.textContent = "This field is mandatory";
+        carValidator = false;
+    }
+    else {
+        inputColorDiv.classList.remove("is-invalid");
+        inputColorError.textContent = "";
+    }
+    if (!validatePlate(inputPlate)) { // ! es igual que == false
+        inputPlateDiv.classList.add("is-invalid");
+        inputPlateError.textContent = "Plate format should be 4 numbers and 3 letters";
+        carValidator = false;
+    }
+    else {
+        inputPlateDiv.classList.remove("is-invalid");
+        inputPlateError.textContent = "";
+    }
+    if (carValidator) {
         car.plate = inputPlate;
         car.color = inputColor;
         car.brand = inputBrand;
         showCar(car);
     }
-    else {
-        inputPlateDiv.classList.add("is-invalid");
-        inputPlateError.textContent = "Plate format should be 4 numbers and 3 letters";
-    }
 }
-function validateBrand() {
+function validateDiam(diams) {
+    var regex = /[0-9.]+$/;
+    if (diams != "" && regex.test(diams)) {
+        var diamsNumerico = parseFloat(diams);
+        if (diamsNumerico >= 0.4 && diamsNumerico <= 2) {
+            return true;
+        }
+    }
+    return false;
 }
 function addWheels(car) {
     var _a, _b, _c, _d;
+    var wheelValidator = true;
     for (var i = 1; i < 5; i++) {
         var marcaRueda = document.getElementById("marcaRueda" + i).value;
-        var diametroRueda = parseFloat(document.getElementById("diametroRueda" + i).value);
+        var diametroRueda = document.getElementById("diametroRueda" + i).value;
         var marcaRuedaErrorDiv = document.getElementById("marcaRuedaError" + i);
         var ruedaErrorDiv = document.getElementById("rueda" + i + "Error");
         (_a = document.getElementById("marcaRueda" + i)) === null || _a === void 0 ? void 0 : _a.classList.remove("is-invalid");
         (_b = document.getElementById("diametroRueda" + i)) === null || _b === void 0 ? void 0 : _b.classList.remove("is-invalid");
-        if (diametroRueda >= 0.4 && diametroRueda <= 2 && !marcaRueda == "") {
+        if (marcaRueda == "") {
+            (_c = document.getElementById("marcaRueda" + i)) === null || _c === void 0 ? void 0 : _c.classList.add("is-invalid");
+            marcaRuedaErrorDiv.textContent = "Brand must be indicated";
+            wheelValidator = false;
+        }
+        if (!validateDiam(diametroRueda)) {
+            (_d = document.getElementById("diametroRueda" + i)) === null || _d === void 0 ? void 0 : _d.classList.add("is-invalid");
+            ruedaErrorDiv.textContent = "Diameter should be between 0.4 and 2";
+            wheelValidator = false;
+        }
+    }
+    if (wheelValidator) {
+        for (var i = 1; i < 5; i++) {
+            var marcaRueda = document.getElementById("marcaRueda" + i).value;
+            var diametroRueda = parseFloat(document.getElementById("diametroRueda" + i).value);
             var newWheel = new Wheel(diametroRueda, marcaRueda);
             car.addWheel(newWheel);
         }
-        else if (marcaRueda == "") {
-            console.log("Error!!!!");
-            (_c = document.getElementById("marcaRueda" + i)) === null || _c === void 0 ? void 0 : _c.classList.add("is-invalid");
-            marcaRuedaErrorDiv.textContent = "Brand must be indicated";
-            return false;
-        }
-        else {
-            (_d = document.getElementById("diametroRueda" + i)) === null || _d === void 0 ? void 0 : _d.classList.add("is-invalid");
-            ruedaErrorDiv.textContent = "Diameter should be between 0.4 and 2";
-            return false;
-        }
+        showWheels(car);
     }
-    showWheels(car);
 }
 var createCarBtnInput = document.getElementById('createCarBtn');
 createCarBtnInput.addEventListener("click", function () {
